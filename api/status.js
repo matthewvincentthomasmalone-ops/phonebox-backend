be145d0bd4bcd3b3ac863ac19f199b61202e20a8
matthewvincentthomasmalone-ops <matthew.vincent.thomas.malone@gmail.com>
2026-02-24 09:45:06 +1000

    Update environment variable names for Redis

--- api/status.js
+++ api/status.js
@@ -5,8 +5,8 @@ module.exports = async (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   // Manually grab variables to ensure they exist
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url = process.env.REDIS_REST_URL;
+  const token = process.env.REDIS_REST_TOKEN;
 
   if (!url || !token) {
     res.statusCode = 500;
